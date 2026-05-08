ffaacaa1930bd18554cd41dbfd3f66d3597d919b
Courey J <coureyfrance@gmail.com>
2026-05-08 15:59:17 -0400

    called persistent manager to save icons/pecs

--- Sign and Says.swiftpm/PECS.swift
+++ Sign and Says.swiftpm/PECS.swift
@@ -215,6 +215,12 @@ struct PECS: View {
         } message: {
             Text("Please tap an icon to make a request.")
         }
+        .onChange(of: icons) {
+            PersistenceManager.savePECS(icons: icons, words: words)
+        }
+        .onChange(of: words) {
+            PersistenceManager.savePECS(icons: icons, words: words)
+        }
     }
     @ViewBuilder
     func DeleteButton(action: @escaping () -> Void) -> some View {
